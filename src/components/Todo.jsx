4dac51d8530ccefe065c94df10ffab39834c1ced
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-04-11 08:35:55 +0100

    Use multiple state slices

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,17 +3,24 @@ import React, { useState } from 'react';
 const todo = props => {
 
   const [todoName, setTodoName] = useState('');
+  const [todoList, setTodoList] = useState([]);
 
   const inputChangedHandler = (event) => {
     setTodoName(event.target.value);
   }
 
+  const todoAddHandler = () => {
+    setTodoList(todoList.concat(todoName));
+  }
+
   return (
     <React.Fragment>
       <input type="text" placeholder="Todo" onChange={inputChangedHandler} value={todoName} />
-      <button type="button" class="btn btn-outline-primary">Add</button>
+      <button type="button" onClick={todoAddHandler} class="btn btn-outline-primary">Add</button>
       <ul>
-        <li>{todoName}</li>
+        {
+          todoList.map(todo => <li>{todo}</li>)
+        }
       </ul>
     </React.Fragment>
   )
