6360e4a2b55632cc03039897e2eef964a9108d94
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-04-11 08:32:05 +0100

    Use array destructing on the useState effect

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 
 const todo = props => {
 
-  const inputState = useState('');
+  const [todoName, setTodoName] = useState('');
 
   const inputChangedHandler = (event) => {
-    inputState[1](event.target.value);
+    setTodoName(event.target.value);
   }
 
   return (
     <React.Fragment>
-      <input type="text" placeholder="Todo" onChange={inputChangedHandler} value={inputState[0]} />
+      <input type="text" placeholder="Todo" onChange={inputChangedHandler} value={todoName} />
       <button type="button" class="btn btn-outline-primary">Add</button>
       <ul>
-        <li>{inputState[0]}</li>
+        <li>{todoName}</li>
       </ul>
     </React.Fragment>
   )
