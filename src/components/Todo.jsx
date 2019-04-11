37a6004b1741dda847d01744a982abec6f8c9601
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-04-11 09:09:48 +0100

    Add axios package & firebase backend database

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const todo = props => {
 
   const [todoName, setTodoName] = useState('');
   const [todoList, setTodoList] = useState([]);
 
+  useEffect(() => {
+    try {
+      axios.get('https://react-hooks-3c5da.firebaseio.com/todos.json')
+        .then(res => {
+          const { data } = res;
+          const todos = [];
+          for (const key in data) {
+            todos.push({ id: key, name: data[key].name });
+          }
+          setTodoList(todos.map(todo => todo.name));
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
   const inputChangedHandler = (event) => {
     setTodoName(event.target.value);
   }
 
-  const todoAddHandler = () => {
-    setTodoList(todoList.concat(todoName));
+  const todoAddHandler = async () => {
+    try {
+      setTodoList(todoList.concat(todoName));
+      const response = await axios.post('https://react-hooks-3c5da.firebaseio.com/todos.json', { name: todoName });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
